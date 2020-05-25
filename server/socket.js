9db861a297856189269db64b08a347bae2d3acab
MingyumKim <Mingyum93@gmail.com>
2020-05-25 21:47:32 +0300

    timeout for initial state offer

--- server/socket.js
+++ server/socket.js
@@ -1,4 +1,5 @@
 const messageDao = require("./messagesDao");
+const stateTransitionDao = require("./statetransitiondao");
 const { uuid } = require("uuidv4");
 
 module.exports = function (server) {
@@ -9,13 +10,13 @@ module.exports = function (server) {
         socket.on("error", () => console.log("Recieved error from client: ", socket.id));
         socket.on("join", (id) => socket.join(id));
         socket.on("message", (message) => {
-            
             message = { ...message, createdAt: new Date(), isRead: 0, id: uuid() }
             messageDao.createNewMessage(message, function (err, data) {
                 if (err) socket.emit("message-error");
                 else {
                     if (message.type === "NOTIFICATION") {
                         socket.broadcast.to(message.receiver).emit("state_changed");
+                        socket.emit("state_changed");
                         if (socket.rooms[message.receiver])
                             socket.emit("message", message); // emit to myself
                         else
@@ -24,9 +25,50 @@ module.exports = function (server) {
                     else{
                         socket.emit("message", message);
                         socket.broadcast.to(message.receiver).emit("message", message);
+                        if(message.type === "OFFER"){
+                            const tenSeconds = 1000 * 10;
+                            setTimeout(()=>{
+                                stateTransitionDao.getCurrentState(message.id, function (err, data){
+                                    if(err) console.log("There was error fetching current state of offer");
+                                    if(data.length === 0){
+                                        const transition = {object_id:message.id, new_state:"canceled", timestamp:new Date()}
+                                        stateTransitionDao.createNewTransition(transition, function(err, data){
+                                            if(err) console.log("There was error creating new state of offer");
+                                            else{
+                                                const timeoutNotificationToRunner = {
+                                                    id:uuid(), createdAt:new Date(), isRead:0, sender:"SYSTEM", receiver:message.sender, relatedUser:message.receiver, 
+                                                    type:"NOTIFICATION", content:`offer id ${message.id} is canceled due to timeout`
+                                                };
+                                                const timeoutNotificationToPoster = {
+                                                    id:uuid(), createdAt:new Date(), isRead:0, sender:"SYSTEM", receiver:message.receiver, relatedUser:message.sender, 
+                                                    type:"NOTIFICATION", content:`offer id ${message.id} is canceled due to timeout`
+                                                };
+                                                messageDao.createNewMessage(timeoutNotificationToRunner, function (err, data){
+                                                    if(err) console.log("There was error creating new timout notification")
+                                                    else {
+                                                        socket.emit("message", timeoutNotificationToRunner);
+                                                        socket.emit("state_changed");
+                                                    }
+                                                });
+                                                messageDao.createNewMessage(timeoutNotificationToPoster, function (err, data){
+                                                    if(err) console.log("There was error creating new timout notification")
+                                                    else {
+                                                        socket.broadcast.to(message.receiver).emit("message", timeoutNotificationToPoster);
+                                                        socket.broadcast.to(message.receiver).emit("state_changed");
+                                                    }
+                                                });
+                                            }
+                                        })
+                                    }
+                                });
+                            }, tenSeconds)
+                        }
                     }
                 };
             });
+        });
+        socket.on("offer-", (object_id, new_state) => {
+            stateTransitionDao.createNewTransition()
         });
     });
 }; 
