954f7c7f6472c8812810a62afc84f3d6e4fbe677
MingyumKim <Mingyum93@gmail.com>
2020-06-03 18:15:10 +0300

    creating history tab in MyErrands page

--- client/src/components/MyErrand.js
+++ client/src/components/MyErrand.js
@@ -2,22 +2,42 @@ import React from "react";
 import socket from "../utils/socket";
 
 function MyErrand({errand, tab, user}){
+    //TODO : make ended errands are not visible with currently activated ones.
+    //     : make input take review from user
+    //     : nice work flow with dialog or confirm  
 
     function changeErrandState(new_state){
         socket.emit("errand-state-transition", {object_id:errand.id, new_state, user_id:user.id});
     };
 
-    if(tab === "poster_id")
-    return <>
-        {errand && <div>{errand.id}</div>}
-        {errand.state === "initial"? <button onClick={()=>changeErrandState("deleted")}>Delete</button> : 
-                                    <><button onClick={()=>changeErrandState("unsuccessful")}>Cancel</button>
-                                      <button onClick={()=>changeErrandState("deleted")}>Finish</button></>}
-    </>
+    function deleteErrand(){
+        //Do you really want to delete?
+        // changeErrandState("deleted");
+    };
 
-    if(tab === "runner_id")
+    function cancelRunning(){
+        //Do you really want to cancel? there will be penalty review
+        // changeErrandState("canceled");
+    };
+
+    function doneByPoster(){
+        //Do you really want to finish?  choose if it was successful or not
+        //please leave review to the runner
+        //changeErrandState("unsuccessful or successful");
+    };
+
+    function doneByRunner(){
+        //Good job! if Poster also finish it, you will get money. (auto finish with timeout)
+        //please leave review to the poster
+        //changeErrandState("Should I have one more state ..? maybe?");
+    }
+
     return <>
-        {errand && <div>{errand.id}<button onClick={()=>changeErrandState("unsuccessful")}>Cancel</button></div>}
+        <br />
+        {errand.id}
+        {tab === "posting" && errand.state === "initial" && <button>Delete</button>}
+        {tab === "posting" && errand.state === "running" && <><button>Cancel</button><button>It's done!</button></>}
+        {tab === "running" && <><button>Cancel</button><button>I did!</button></>}
     </>
 };
 
--- client/src/pages/MyErrands.js
+++ client/src/pages/MyErrands.js
@@ -6,7 +6,8 @@ import MyErrand from "../components/MyErrand";
 function MyErrands({user}){
 
     const [myErrands, setMyErrands] = useState(null);
-    const [tab, setTab] = useState("poster_id");
+    const [tab, setTab] = useState("posting");
+    const [errandsToShow, setErrandsToShow] = useState(null);
 
     useEffect(()=>{
         if(user)
@@ -15,13 +16,31 @@ function MyErrands({user}){
         }
     },[user]);
 
+    useEffect(()=>{
+        if(myErrands && tab){
+            if(tab === "history"){
+                const terminatedErrands = myErrands.filter(errand => errand.state === "unsuccessful" || errand.state === "successful" || errand.state === "deleted");
+                setErrandsToShow(terminatedErrands);
+            }
+            else if(tab === "posting"){
+                const myActivatedPosting = myErrands.filter(errand => (errand.state === "initial" || errand.state === "running") && errand.poster_id === user.id);
+                setErrandsToShow(myActivatedPosting);
+            }
+            else if(tab === "running"){
+                const myActivatedRunning = myErrands.filter(errand => errand.state === "running" && errand.runner_id === user.id);
+                setErrandsToShow(myActivatedRunning);
+            }
+        }
+    }, [myErrands, tab, user]);
+
     return(
-        <>
+        <> 
             <br/>
-            <button value="poster_id" onClick={(e)=>setTab(e.target.value)}>Posting</button>
-            <button value="runner_id" onClick={(e)=>setTab(e.target.value)}>Running</button>
+            <button value="posting" onClick={(e)=>setTab(e.target.value)}>Posting</button>
+            <button value="running" onClick={(e)=>setTab(e.target.value)}>Running</button>
+            <button value="history" onClick={(e)=>setTab(e.target.value)}>History</button>
             <button onClick={()=>console.log(myErrands)}>see my errands</button>
-            {myErrands && myErrands.filter(errand => errand[tab] === user.id).map(errand => <MyErrand key={errand.id} errand={errand} tab={tab} user={user} />)}
+            {errandsToShow && errandsToShow.map(errand => <MyErrand key={errand.id} errand={errand} tab={tab} user={user} />)}
         </>
     );
 };
