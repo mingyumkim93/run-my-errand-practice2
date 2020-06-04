3b66ff90e17355b09aca7eec21f225d72e60d5b0
MingyumKim <Mingyum93@gmail.com>
2020-06-04 17:32:35 +0300

    handling my errand in client side

--- client/src/components/MyErrand.js
+++ client/src/components/MyErrand.js
@@ -2,42 +2,50 @@ import React from "react";
 import socket from "../utils/socket";
 
 function MyErrand({errand, tab, user}){
-    //TODO : make ended errands are not visible with currently activated ones.
-    //     : make input take review from user
-    //     : nice work flow with dialog or confirm  
 
     function changeErrandState(new_state){
         socket.emit("errand-state-transition", {object_id:errand.id, new_state, user_id:user.id});
     };
 
     function deleteErrand(){
-        //Do you really want to delete?
-        // changeErrandState("deleted");
+        const confirmDelete = window.confirm("Do you really want to finish it?");
+        if(confirmDelete)  changeErrandState("deleted");
     };
 
     function cancelRunning(){
-        //Do you really want to cancel? there will be penalty review
-        // changeErrandState("canceled");
+        const confirmCancel = window.confirm("Do you really want to cancel? There will be panalty review!");
+        if(confirmCancel) changeErrandState("canceled");
     };
 
     function doneByPoster(){
-        //Do you really want to finish?  choose if it was successful or not
-        //please leave review to the runner
-        //changeErrandState("unsuccessful or successful");
+        const isSuccessful = window.confirm("Did runner do good job?");
+        const reviewRating = window.prompt("Give rating to the runner");
+        const reviewComment = window.prompt("Give a comment");
+        const confirmDone = window.confirm(`Your answer: ${isSuccessful ? "Successful":"Unsuccessful"}, ${reviewRating} rating, comment: ${reviewComment}. 
+                                        Are you really sure you want to submit it?`);
+        if(confirmDone) {
+            changeErrandState(isSuccessful? "successful" : "unsuccessful");
+            //create review
+        }
     };
 
     function doneByRunner(){
-        //Good job! if Poster also finish it, you will get money. (auto finish with timeout)
-        //please leave review to the poster
-        //changeErrandState("Should I have one more state ..? maybe?");
+        window.alert("Good job! Once poster also confirm it, you will get your money. (There is timeout)");
+        const reviewRating = window.prompt("Give rating to the poster");
+        const reviewComment = window.prompt("Give a comment");
+        const confirmDone = window.confirm(`Your review to poster: ${reviewRating} rating, comment: ${reviewComment}. 
+                                        Are you really sure you want to submit it?`);
+        if(confirmDone){
+            changeErrandState("doneByRunner");
+        }
     }
 
     return <>
         <br />
         {errand.id}
-        {tab === "posting" && errand.state === "initial" && <button>Delete</button>}
-        {tab === "posting" && errand.state === "running" && <><button>Cancel</button><button>It's done!</button></>}
-        {tab === "running" && <><button>Cancel</button><button>I did!</button></>}
+        {tab === "posting" && errand.state === "initial" && <button onClick={()=>deleteErrand()}>Delete</button>}
+        {tab === "posting" && errand.state === "running" && <><button onClick={()=>cancelRunning()}>Cancel</button><button onClick={()=>doneByPoster()}>It's done!</button></>}
+        {tab === "running" && <><button onClick={()=>cancelRunning()}>Cancel</button><button onClick={()=>doneByRunner()}>I did!</button></>}
     </>
 };
 
